4c3e8f5b4d65fa30439d928bdb7b515028fe31b6
Joshua Morin <73865022+TheRevBv@users.noreply.github.com>
2024-11-22 11:39:00 -0600

    Login complete

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,6 +10,7 @@ import {
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { LoginUserDto } from './dto/login-user-dto';
 
 @Controller('auth')
 export class AuthController {
@@ -18,6 +19,11 @@ export class AuthController {
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.authService.create(createUserDto);
+  }
+
+  @Post('/login')
+  login(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto);
   }
 
   @Get()
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,22 +2,57 @@ import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import * as bcrypt from 'bcryptjs';
 
+import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
-import { User } from './entities/user.entity';
+import { LoginUserDto } from './dto/login-user-dto';
+import { JwtPayload } from './interfaces/jwt-payload';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(User.name)
     private userModel: Model<User>,
+    private jwtSvc: JwtService,
   ) {}
+
+  async login(loginUserDto: LoginUserDto): Promise<any> {
+    const { email, password } = loginUserDto;
+
+    try {
+      const user = await this.userModel.findOne({ email });
+      if (!user) {
+        throw new UnauthorizedException(
+          `Credenciales no validas para ${email}`,
+        );
+      }
+
+      if (!bcrypt.compareSync(password, user.password)) {
+        throw new UnauthorizedException(
+          `Credenciales no validas para contraseña`,
+        );
+      }
+
+      const { password: _, ...validUser } = user.toJSON();
+
+      return {
+        user: validUser,
+        token: await this.getJwtToken({ id: user.id }),
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `Algo extraño ha pasado, intente despues (error): ${error}`,
+      );
+    }
+  }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     console.log(createUserDto);
@@ -60,5 +95,10 @@ export class AuthService {
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
+  }
+
+  async getJwtToken(payload: JwtPayload) {
+    const token = await this.jwtSvc.signAsync(payload);
+    return token;
   }
 }
