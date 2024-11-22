8a711dce7744da3eb7e7791e026162f2f13ab855
Joshua Morin <73865022+TheRevBv@users.noreply.github.com>
2024-11-22 09:35:31 -0600

    Agrega usuarios

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,49 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import * as bcrypt from 'bcryptjs';
+
+import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    console.log(createUserDto);
+    try {
+      const { password, ...userData } = createUserDto;
+
+      const newUser = new this.userModel({
+        password: bcrypt.hashSync(password, 10),
+        ...userData,
+      });
+
+      await newUser.save();
+
+      const { password: _, ...user } = newUser.toJSON();
+
+      return user;
+    } catch (error) {
+      // console.error(error);
+      if (error.code === 11000) {
+        throw new BadRequestException(`${createUserDto.email} ya existe`);
+      }
+
+      throw new InternalServerErrorException(
+        `Algo extraño ha pasado, intente despues (error): ${error}`,
+      );
+    }
   }
 
   findAll() {
